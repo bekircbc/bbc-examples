917d2a72f0b85141fc9dab850c2a65d5f3d7c404
Bekir - CDI <b.burakcebecii@gmail.com>
2022-12-31 17:15:40 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+class HelloMessage extends React.Component {
+  render() {
+    return <div>Merhaba {this.props.name}</div>;
+  }
+}
+
+ReactDOM.render(
+  <HelloMessage name="Ali" />,
+  document.getElementById("hello-example")
+);
